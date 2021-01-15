60735ccff15cb1bc21483dae278dc6e687e1ecc0
zozospider <2642220896@qq.com>
2021-01-15 18:52:54 +0800

    Update Play-with-Linear-Algebra

--- 05-More-about-Matrix/06-Implement-Identity-Matrix-and-Inverse-of-a-Matrix-in-Numpy/main_numpy_matrix.py
+++ 05-More-about-Matrix/06-Implement-Identity-Matrix-and-Inverse-of-a-Matrix-in-Numpy/main_numpy_matrix.py
@@ -4,72 +4,79 @@ if __name__ == "__main__":
     print(np.__version__)
 
     # 创建
-    M1 = np.array([[1, 2], [3, 4]])
+    matrix1 = np.array([[1, 2], [3, 4]])
     # [[1 2]
     #  [3 4]]
-    print(M1)
-    M2 = np.array([[10, 20], [30, 40]])
+    print(matrix1)
+    matrix2 = np.array([[10, 20], [30, 40]])
     # [[10 20]
     #  [30 40]]
-    print(M2)
-    V = np.array([1, 10])
+    print(matrix2)
+    vector = np.array([1, 10])
     # [ 1 10]
-    print(V)
+    print(vector)
 
     # 形状
     # (2, 2)
-    print(M1.shape)
+    print(matrix1.shape)
 
     # 转置
     # [[1 3]
     #  [2 4]]
-    print(M1.T)
+    print(matrix1.T)
 
     # 元素
     # 4
-    print(M1[1, 1])
+    print(matrix1[1, 1])
 
     # 行向量
     # [1 2]
-    print(M1[0])
+    print(matrix1[0])
     # [1 2]
-    print(M1[0, :])
+    print(matrix1[0, :])
 
     # 列向量
     # [1 3]
-    print(M1[:, 0])
+    print(matrix1[:, 0])
 
     # 运算
     # [[11 22]
     #  [33 44]]
-    print(M1 + M2)
+    print(matrix1 + matrix2)
     # [[2 3]
     #  [4 5]]
-    print(M1 + 1)
+    print(matrix1 + 1)
     # [[ -9 -18]
     #  [-27 -36]]
-    print(M1 - M2)
+    print(matrix1 - matrix2)
     # [[0 1]
     #  [2 3]]
-    print(M1 - 1)
+    print(matrix1 - 1)
     # [[10 20]
     #  [30 40]]
-    print(M1 * 10)
+    print(matrix1 * 10)
     # [[10 20]
     #  [30 40]]
-    print(10 * M1)
+    print(10 * matrix1)
     # [[ 10  40]
     #  [ 90 160]]
-    print(M1 * M2)
+    print(matrix1 * matrix2)
     # [[ 70 100]
     #  [150 220]]
-    print(M1.dot(M2))
+    print(matrix1.dot(matrix2))
 
     # [[ 2 12]
     #  [ 4 14]]
-    print(M1 + V)
+    print(matrix1 + vector)
     # [[ 0 -8]
     #  [ 2 -6]]
-    print(M1 - V)
+    print(matrix1 - vector)
     # [21 43]
-    print(M1.dot(V))
+    print(matrix1.dot(vector))
+
+    # 单位矩阵
+    # [[1. 0. 0.]
+    #  [0. 1. 0.]
+    #  [0. 0. 1.]]
+    identity_3 = np.identity(3)
+    print(identity_3)
