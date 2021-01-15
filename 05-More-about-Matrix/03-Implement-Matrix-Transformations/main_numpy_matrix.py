fb0449e6a4767a6dda955e966599184e32966479
zozospider <2642220896@qq.com>
2021-01-15 16:44:11 +0800

    Update Play-with-Linear-Algebra

--- 05-More-about-Matrix/03-Implement-Matrix-Transformations/main_numpy_matrix.py
+++ 05-More-about-Matrix/03-Implement-Matrix-Transformations/main_numpy_matrix.py
@@ -4,72 +4,72 @@ if __name__ == "__main__":
     print(np.__version__)
 
     # 创建
-    A = np.array([[1, 2], [3, 4]])
+    M1 = np.array([[1, 2], [3, 4]])
     # [[1 2]
     #  [3 4]]
-    print(A)
-    B = np.array([[10, 20], [30, 40]])
+    print(M1)
+    M2 = np.array([[10, 20], [30, 40]])
     # [[10 20]
     #  [30 40]]
-    print(B)
-    v = np.array([1, 10])
+    print(M2)
+    V = np.array([1, 10])
     # [ 1 10]
-    print(v)
+    print(V)
 
     # 形状
     # (2, 2)
-    print(A.shape)
+    print(M1.shape)
 
     # 转置
     # [[1 3]
     #  [2 4]]
-    print(A.T)
+    print(M1.T)
 
     # 元素
     # 4
-    print(A[1, 1])
+    print(M1[1, 1])
 
     # 行向量
     # [1 2]
-    print(A[0])
+    print(M1[0])
     # [1 2]
-    print(A[0, :])
+    print(M1[0, :])
 
     # 列向量
     # [1 3]
-    print(A[:, 0])
+    print(M1[:, 0])
 
     # 运算
     # [[11 22]
     #  [33 44]]
-    print(A + B)
+    print(M1 + M2)
     # [[2 3]
     #  [4 5]]
-    print(A + 1)
+    print(M1 + 1)
     # [[ -9 -18]
     #  [-27 -36]]
-    print(A - B)
+    print(M1 - M2)
     # [[0 1]
     #  [2 3]]
-    print(A - 1)
+    print(M1 - 1)
     # [[10 20]
     #  [30 40]]
-    print(A * 10)
+    print(M1 * 10)
     # [[10 20]
     #  [30 40]]
-    print(10 * A)
+    print(10 * M1)
     # [[ 10  40]
     #  [ 90 160]]
-    print(A * B)
+    print(M1 * M2)
     # [[ 70 100]
     #  [150 220]]
-    print(A.dot(B))
+    print(M1.dot(M2))
 
     # [[ 2 12]
     #  [ 4 14]]
-    print(A + v)
+    print(M1 + V)
     # [[ 0 -8]
     #  [ 2 -6]]
-    print(A - v)
+    print(M1 - V)
     # [21 43]
-    print(A.dot(v))
+    print(M1.dot(V))
